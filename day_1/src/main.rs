f9f1ce5efc5f35d189b50434395c66c15191c160
Tim Hoiberg <tim.hoiberg@gmail.com>
2019-12-05 22:57:44 +1100

    Add solution for second half

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -4,12 +4,29 @@ use std::io::prelude::*;
 fn main() {
     // TODO fix file pathing. Relative to the cwd, not this file itself
     let input_data = read_input_data_from_file("src/example_data").unwrap();
-    let total_fuel_needed: i32 = input_data
-        .into_iter()
-        .map(|module_mass| fuel_for_mass(module_mass))
-        .fold(0, |acc, x| acc + x);
 
-    println!("{:?}", total_fuel_needed);
+    println!(
+        "The part one solution is: {:?}",
+        part_one_solution(&input_data)
+    );
+    println!(
+        "The part two solution is: {:?}",
+        part_two_solution(&input_data)
+    )
+}
+
+fn part_one_solution(input_data: &Vec<i32>) -> i32 {
+    return input_data
+        .into_iter()
+        .map(|module_mass| fuel_for_mass(*module_mass))
+        .fold(0, |acc, x| acc + x);
+}
+
+fn part_two_solution(input_data: &Vec<i32>) -> i32 {
+    return input_data
+        .into_iter()
+        .map(|module_mass| recursive_fuel_for_mass(*module_mass))
+        .fold(0, |acc, x| acc + x);
 }
 
 fn read_input_data_from_file(filepath: &str) -> Result<Vec<i32>, std::io::Error> {
@@ -29,6 +46,20 @@ fn fuel_for_mass(module: i32) -> i32 {
     return module / 3 - 2;
 }
 
+fn recursive_fuel_for_mass(module: i32) -> i32 {
+    let mut fuels: Vec<i32> = vec![];
+    let mut fuel_remaining = module;
+
+    while fuel_remaining > 0 {
+        fuel_remaining = fuel_for_mass(fuel_remaining);
+        if fuel_remaining > 0 {
+            fuels.push(fuel_remaining);
+        }
+    }
+
+    return fuels.into_iter().fold(0, |acc, fuel| acc + fuel);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -39,5 +70,12 @@ mod tests {
         assert_eq!(fuel_for_mass(14), 2);
         assert_eq!(fuel_for_mass(1969), 654);
         assert_eq!(fuel_for_mass(100756), 33583);
+    }
+
+    #[test]
+    fn test_recursive_fuel_for_mass() {
+        assert_eq!(recursive_fuel_for_mass(14), 2);
+        assert_eq!(recursive_fuel_for_mass(1969), 966);
+        assert_eq!(recursive_fuel_for_mass(100756), 50346);
     }
 }
