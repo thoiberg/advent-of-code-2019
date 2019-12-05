35710ba39c142052a6c66bf5f6940f11ab45e175
Tim Hoiberg <tim.hoiberg@gmail.com>
2019-12-05 21:01:30 +1100

    Read input data from file

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,5 +1,10 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    let total_fuel_needed: i32 = example_data()
+    // TODO fix file pathing. Relative to the cwd, not this file itself
+    let input_data = read_input_data_from_file("src/example_data").unwrap();
+    let total_fuel_needed: i32 = input_data
         .into_iter()
         .map(|module_mass| fuel_for_mass(module_mass))
         .fold(0, |acc, x| acc + x);
@@ -7,18 +12,17 @@ fn main() {
     println!("{:?}", total_fuel_needed);
 }
 
-fn example_data() -> Vec<i32> {
-    return vec![
-        120847, 60347, 63340, 72773, 57020, 133960, 98163, 121548, 87233, 59150, 59712, 146816,
-        93205, 61936, 75319, 141998, 97125, 73450, 106250, 129939, 94854, 113782, 112044, 127923,
-        67114, 119770, 130034, 70876, 82204, 101939, 132604, 142836, 117066, 95861, 75597, 94630,
-        50085, 101107, 77937, 94286, 74091, 140875, 118543, 52767, 54544, 93062, 115681, 142065,
-        111737, 131214, 75160, 115432, 140504, 115376, 86589, 104631, 133012, 108690, 85993, 99533,
-        133725, 87698, 133480, 68379, 107852, 111209, 116623, 98717, 130227, 114356, 144516, 89663,
-        118355, 77816, 149914, 105080, 116793, 65259, 143900, 67838, 148389, 129753, 140524, 90005,
-        147305, 118428, 79940, 59110, 78120, 87066, 64722, 142066, 81410, 106958, 92984, 95584,
-        108534, 66362, 126340, 143660,
-    ];
+fn read_input_data_from_file(filepath: &str) -> Result<Vec<i32>, std::io::Error> {
+    let mut raw_input_data = File::open(filepath)?;
+    let mut contents = String::new();
+    raw_input_data.read_to_string(&mut contents)?;
+
+    let split_by_line = contents
+        .split("\n")
+        .filter_map(|line| line.parse::<i32>().ok())
+        .collect();
+
+    Ok(split_by_line)
 }
 
 fn fuel_for_mass(module: i32) -> i32 {
