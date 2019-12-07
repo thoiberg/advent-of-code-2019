5f06e78973c1e3eddfb4728107908bb874c6c45a
Tim Hoiberg <tim.hoiberg@gmail.com>
2019-12-07 22:52:09 +1100

    Break methods out into lib.rs

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,6 +1,4 @@
-use std::fs::File;
-use std::io;
-use std::io::Read;
+use advent_of_code_2019_day_2::*;
 
 fn main() {
     println!("Part one: {:?}", solve_part_one());
@@ -30,117 +28,9 @@ fn solve_part_two() -> Option<i32> {
     return Some(noun * 100 + verb);
 }
 
-fn find_noun_and_verb_that_match_output(
-    intcodes: Vec<i32>,
-    expected_output: i32,
-) -> Option<(i32, i32)> {
-    for noun in 0..100 {
-        for verb in 0..100 {
-            let mut clean_intcodes = intcodes.clone();
-            clean_intcodes[1] = noun;
-            clean_intcodes[2] = verb;
-
-            let instruction_pointer = process_intcodes(clean_intcodes)[0];
-            if instruction_pointer == expected_output {
-                return Some((noun, verb));
-            }
-        }
-    }
-
-    return None;
-}
-
-fn process_intcodes(mut intcodes: Vec<i32>) -> Vec<i32> {
-    for opcode_position in (0..intcodes.len()).step_by(4) {
-        let opcode = intcodes[opcode_position];
-
-        match opcode {
-            1 => {
-                let (position_to_insert, new_value) = new_value_and_position_to_insert(
-                    opcode_position,
-                    &intcodes,
-                    OpcodeActions::Add,
-                );
-                intcodes[position_to_insert] = new_value;
-            }
-            2 => {
-                let (position_to_insert, new_value) = new_value_and_position_to_insert(
-                    opcode_position,
-                    &intcodes,
-                    OpcodeActions::Multiply,
-                );
-                intcodes[position_to_insert] = new_value;
-            }
-            99 => break,
-            _ => panic!("Unexpected Opcode, exiting"),
-        }
-    }
-
-    intcodes
-}
-
-enum OpcodeActions {
-    Add,
-    Multiply,
-}
-
-fn new_value_and_position_to_insert(
-    opcode_position: usize,
-    intcodes: &Vec<i32>,
-    action: OpcodeActions,
-) -> (usize, i32) {
-    let first_value_index = intcodes[opcode_position + 1] as usize;
-    let second_value_index = intcodes[opcode_position + 2] as usize;
-    let position_to_insert_new_value = intcodes[opcode_position + 3] as usize;
-
-    let new_value = match action {
-        OpcodeActions::Add => intcodes[first_value_index] + intcodes[second_value_index],
-        OpcodeActions::Multiply => intcodes[first_value_index] * intcodes[second_value_index],
-    };
-
-    return (position_to_insert_new_value, new_value);
-}
-
-fn read_input_data_from_file(filepath: String) -> Result<Vec<i32>, io::Error> {
-    let mut raw_input_data = File::open(filepath)?;
-    let mut contents = String::new();
-    raw_input_data.read_to_string(&mut contents)?;
-
-    Ok(contents
-        .split(",")
-        .filter_map(|int| int.parse::<i32>().ok())
-        .collect())
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_process_intcode_adds_correctly() {
-        assert_eq!(process_intcodes(vec![1, 0, 0, 0, 99]), vec![2, 0, 0, 0, 99]);
-    }
-
-    #[test]
-    fn test_process_intcode_multiplies_correctly() {
-        assert_eq!(process_intcodes(vec![2, 3, 0, 3, 99]), vec![2, 3, 0, 6, 99]);
-    }
-
-    #[test]
-    fn test_process_intcodes_breaks_on_exit_code() {
-        assert_eq!(
-            process_intcodes(vec![2, 4, 4, 5, 99, 0]),
-            vec![2, 4, 4, 5, 99, 9801]
-        );
-    }
-
-    #[test]
-    fn test_process_intcodes_can_perform_multiple_actions() {
-        assert_eq!(
-            process_intcodes(vec![1, 1, 1, 4, 99, 5, 6, 0, 99]),
-            vec![30, 1, 1, 4, 2, 5, 6, 0, 99]
-        );
-    }
 
     #[test]
     fn test_part_one_solution_is_correct() {
