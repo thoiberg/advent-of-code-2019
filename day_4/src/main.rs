181d3f6c3f712992878f8bf7f4fc71947fb28b6a
Tim Hoiberg <tim.hoiberg@gmail.com>
2019-12-23 22:22:59 +1000

    Create skeleton for day 4

---
+++ day_4/src/main.rs
@@ -0,0 +1,10 @@
+use std::ops::Range;
+
+fn main() {
+    let input_data = input_data();
+    println!("input data range: {:?}", input_data);
+}
+
+fn input_data() -> Range<u32> {
+    172930..683082
+}
